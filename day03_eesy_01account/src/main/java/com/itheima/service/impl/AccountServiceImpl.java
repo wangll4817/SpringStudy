22c21af20278132cfe847c253bb64215f7599dbf
wll <464553387@qq.com>
2019-12-09 10:50:15 +0800

    增加

--- day03_eesy_01account/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
+++ day03_eesy_01account/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.itheima.service.impl;
 import com.itheima.damain.Account;
 import com.itheima.dao.IAccountDao;
 import com.itheima.service.IAccountService;
+import com.itheima.utils.TransactionManager;
 
 import java.util.List;
 
@@ -16,6 +17,11 @@ import java.util.List;
 public class AccountServiceImpl implements IAccountService {
 
     private IAccountDao accountDao;
+    private TransactionManager txManager;
+
+    public void setTxManager(TransactionManager txManager) {
+        this.txManager = txManager;
+    }
 
     public void setAccountDao(IAccountDao accountDao) {
         this.accountDao = accountDao;
@@ -23,10 +29,12 @@ public class AccountServiceImpl implements IAccountService {
 
     public List<Account> findAllAccount() {
         return accountDao.findAllAccount();
+
     }
 
     public Account findAccountById(Integer accountId) {
         return accountDao.findAccountById(accountId);
+
     }
 
     public void saveAccount(Account account) {
@@ -42,17 +50,20 @@ public class AccountServiceImpl implements IAccountService {
     }
 
     public void transfer(String sourceName, String targetName, Float money) {
-        //1.根据名称查询转出账户
-        Account source = accountDao.findAccountByName(sourceName);
-        //2.根据名称查询转入账户
-        Account target = accountDao.findAccountByName(targetName);
-        //3.转出账户减钱
-        source.setMoney(source.getMoney() - money);
-        //4.转入账户加钱
-        target.setMoney(target.getMoney() + money);
-        //5.更新转出账户
-        accountDao.updateAccount(source);
-        //6.更新转入账户
-        accountDao.updateAccount(target);
+            //2.执行操作
+            //2.1.根据名称查询转出账户
+            Account source = accountDao.findAccountByName(sourceName);
+            //2.2根据名称查询转入账户
+            Account target = accountDao.findAccountByName(targetName);
+            //2.3.转出账户减钱
+            source.setMoney(source.getMoney() - money);
+            //2.4.转入账户加钱
+            target.setMoney(target.getMoney() + money);
+            //2.5.更新转出账户
+            accountDao.updateAccount(source);
+            int i = 1/0;
+            //2.6.更新转入账户
+            accountDao.updateAccount(target);
+
     }
 }
