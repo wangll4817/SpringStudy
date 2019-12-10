01f35339117564c09a1b8545661623d23c62f82a
wll <464553387@qq.com>
2019-12-10 09:39:01 +0800

    增加

---
+++ day03_eesy_05annotationAOP/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,25 @@
+package com.itheima.service.impl;
+
+import com.itheima.service.IAccountService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @description:账户的业务层实现类
+ * @author: wll
+ * @create: 2019-12-09 17:27
+ **/
+@Service("accountService")
+public class AccountServiceImpl implements IAccountService {
+    public void saveAccount() {
+        System.out.println("执行了保存");
+    }
+
+    public void updateAccount(int i) {
+        System.out.println("执行了修改" + i);
+    }
+
+    public int deleteAccount() {
+        System.out.println("执行了删除");
+        return 0;
+    }
+}
